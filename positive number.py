dcd10b8b91827c004081fea26d90330b8c26ce32
debu117 <96744623+debu117@users.noreply.github.com>
2022-06-11 12:09:45 +0530

    positive number.py
    This file only print positive number on the screen!

---
+++ positive number.py
@@ -0,0 +1,10 @@
+#Input list1
+list=[12,-7,5,64,-14]
+for num in list:
+    if num>=0:
+        print(num, end=" ")
+#input list2
+list=[12,14,-95,3]
+for num in list:
+    if num>=0:
+        print(num, end=" ")
